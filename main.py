4176e4b12e6d1b608ce4bc660f4377d7772e6876
Koninika <b21ec034@nitm.ac.in>
2023-04-18 14:08:59 +0530

    updating code

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ class Player(pygame.sprite.Sprite):
     def move_left(self,vel):
         self.x_vel=-vel
         if self.direction!="left":
-            self.direction=left
+            self.direction="left"
             self.animation_count=0
         
         
     def move_right(self,vel):
-        self.y_vel=vel
+        self.x_vel=vel
         if self.direction!="right":
-            self.direction=right
+            self.direction="right"
             self.animation_count=0
             
     def loop(self,fps):
@@ -72,7 +72,15 @@ def draw(window,background,bg_image,player):
     
     player.draw(window)
     pygame.display.update()     
-        
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+    player.x_vel=0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+            
     
 
 def main(window):
@@ -89,6 +97,8 @@ def main(window):
             if event.type==pygame.QUIT:
                 run=False
                 break
+        player.loop(FPS)
+        handle_move(player)
         draw(window,background,bg_image,player)
     pygame.quit()
     quit()        
